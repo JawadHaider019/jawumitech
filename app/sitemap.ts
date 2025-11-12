c2290b724ab7cfbd2a3e43aedbb9cd54012e22c8
JawadHaider019 <it.xjawad0019@gmail.com>
2025-11-12 17:26:50 +0500

    updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,25 +5,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://jawumitech.com',
       lastModified: new Date(),
-      changeFrequency: 'monthly' as const,
+      changeFrequency: 'monthly',
       priority: 1,
     },
     {
       url: 'https://jawumitech.com/services',
       lastModified: new Date(),
-      changeFrequency: 'monthly' as const,
+      changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: 'https://jawumitech.com/about',
       lastModified: new Date(),
-      changeFrequency: 'monthly' as const,
+      changeFrequency: 'monthly',
       priority: 0.5,
     },
     {
       url: 'https://jawumitech.com/contact',
       lastModified: new Date(),
-      changeFrequency: 'monthly' as const,
+      changeFrequency: 'monthly',
       priority: 0.5,
     },
   ]
